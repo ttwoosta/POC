1b0b5f7ce509bf902cff55c276b0396f0bcb178a
Tu Tong <ttong@redhawkis.com>
2018-09-21 12:41:04 -0400

    Initial commit.

---
+++ POC.API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace POC.API.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ POC.DataModel/POCXml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POC.DataModel
+{
+    public class POCXml
+    {
+        public int Xml_id { get; set; }
+        public int Xml_TypeId { get; set; }
+        public string XmlFile { get; set; }
+
+    }
+}

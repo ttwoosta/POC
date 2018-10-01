b61118cbe1fa407f5fd459719ccec99d8d075861
Sujan Prajapati <sprajapati@redhawkis.com>
2018-10-01 15:45:32 -0400

    deleted unwanted files

--- POC.API/App_Start/RouteConfig.cs
+++ POC.API/App_Start/RouteConfig.cs
@@ -11,8 +11,10 @@ namespace POC.API
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapMvcAttributeRoutes();
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
